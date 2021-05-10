0328143efeae596ab9b783245fbc442414395719
Firas Qutishat <firas.qutishat@securekey.com>
2021-05-10 12:19:26 -0400

    feat: add update did cmd
    Signed-off-by: Firas Qutishat <firas.qutishat@securekey.com>

---
+++ cmd/orb-cli/updatedidcmd/updatedid.go
@@ -0,0 +1,280 @@
+/*
+Copyright SecureKey Technologies Inc. All Rights Reserved.
+SPDX-License-Identifier: Apache-2.0
+*/
+
+package updatedidcmd
+
+import (
+	"crypto"
+	"crypto/tls"
+	"crypto/x509"
+	"fmt"
+	"strconv"
+
+	"github.com/hyperledger/aries-framework-go-ext/component/vdr/orb"
+	ariesdid "github.com/hyperledger/aries-framework-go/pkg/doc/did"
+	vdrapi "github.com/hyperledger/aries-framework-go/pkg/framework/aries/api/vdr"
+	"github.com/spf13/cobra"
+	cmdutils "github.com/trustbloc/edge-core/pkg/utils/cmd"
+	tlsutils "github.com/trustbloc/edge-core/pkg/utils/tls"
+
+	"github.com/trustbloc/orb/cmd/orb-cli/common"
+)
+
+const (
+	didURIFlagName  = "did-uri"
+	didURIEnvKey    = "ORB_CLI_DID_URI"
+	didURIFlagUsage = "DID URI. " +
+		" Alternatively, this can be set with the following environment variable: " + didURIEnvKey
+
+	domainFlagName      = "domain"
+	domainFileEnvKey    = "ORB_CLI_DOMAIN"
+	domainFileFlagUsage = "URL to the did:orb domain. " +
+		" Alternatively, this can be set with the following environment variable: " + domainFileEnvKey
+
+	sidetreeURLFlagName  = "sidetree-url"
+	sidetreeURLFlagUsage = "Comma-Separated list of sidetree url." +
+		" Alternatively, this can be set with the following environment variable: " + sidetreeURLEnvKey
+	sidetreeURLEnvKey = "ORB_CLI_SIDETREE_URL"
+
+	tlsSystemCertPoolFlagName  = "tls-systemcertpool"
+	tlsSystemCertPoolFlagUsage = "Use system certificate pool." +
+		" Possible values [true] [false]. Defaults to false if not set." +
+		" Alternatively, this can be set with the following environment variable: " + tlsSystemCertPoolEnvKey
+	tlsSystemCertPoolEnvKey = "ORB_CLI_TLS_SYSTEMCERTPOOL"
+
+	tlsCACertsFlagName  = "tls-cacerts"
+	tlsCACertsFlagUsage = "Comma-Separated list of ca certs path." +
+		" Alternatively, this can be set with the following environment variable: " + tlsCACertsEnvKey
+	tlsCACertsEnvKey = "ORB_CLI_TLS_CACERTS"
+
+	sidetreeWriteTokenFlagName  = "sidetree-write-token"
+	sidetreeWriteTokenEnvKey    = "ORB_CLI_SIDETREE_WRITE_TOKEN" //nolint: gosec
+	sidetreeWriteTokenFlagUsage = "The sidetree write token " +
+		" Alternatively, this can be set with the following environment variable: " + sidetreeWriteTokenEnvKey
+
+	addPublicKeyFileFlagName  = "add-publickey-file"
+	addPublicKeyFileEnvKey    = "ORB_CLI_ADD_PUBLICKEY_FILE"
+	addPublicKeyFileFlagUsage = "publickey file include public keys to be added for Orb DID " +
+		" Alternatively, this can be set with the following environment variable: " + addPublicKeyFileEnvKey
+
+	addServiceFileFlagName = "add-service-file"
+	addServiceFileEnvKey   = "ORB_CLI_ADD_SERVICE_FILE"
+	addServiceFlagUsage    = "publickey file include services to be added for Orb DID " +
+		" Alternatively, this can be set with the following environment variable: " + addServiceFileEnvKey
+
+	signingKeyFlagName  = "signingkey"
+	signingKeyEnvKey    = "ORB_CLI_SIGNINGKEY"
+	signingKeyFlagUsage = "The private key PEM used for signing the update of the document." +
+		" Alternatively, this can be set with the following environment variable: " + signingKeyEnvKey
+
+	signingKeyFileFlagName  = "signingkey-file"
+	signingKeyFileEnvKey    = "ORB_CLI_SIGNINGKEY_FILE"
+	signingKeyFileFlagUsage = "The file that contains the private key" +
+		" PEM used for signing the update of the document." +
+		" Alternatively, this can be set with the following environment variable: " + signingKeyFileEnvKey
+
+	signingKeyPasswordFlagName  = "signingkey-password"
+	signingKeyPasswordEnvKey    = "ORB_CLI_SIGNINGKEY_PASSWORD" //nolint: gosec
+	signingKeyPasswordFlagUsage = "signing key pem password. " +
+		" Alternatively, this can be set with the following environment variable: " + signingKeyPasswordEnvKey
+
+	nextUpdateKeyFlagName  = "nextupdatekey"
+	nextUpdateKeyEnvKey    = "ORB_CLI_NEXTUPDATEKEY"
+	nextUpdateKeyFlagUsage = "The public key PEM used for validating the signature of the next update of the document." +
+		" Alternatively, this can be set with the following environment variable: " + nextUpdateKeyEnvKey
+
+	nextUpdateKeyFileFlagName  = "nextupdatekey-file"
+	nextUpdateKeyFileEnvKey    = "ORB_CLI_NEXTUPDATEKEY_FILE"
+	nextUpdateKeyFileFlagUsage = "The file that contains the public key" +
+		" PEM used for validating the signature of the next update of the document. " +
+		" Alternatively, this can be set with the following environment variable: " + nextUpdateKeyFileEnvKey
+)
+
+// GetUpdateDIDCmd returns the Cobra update did command.
+func GetUpdateDIDCmd() *cobra.Command {
+	updateDIDCmd := updateDIDCmd()
+
+	createFlags(updateDIDCmd)
+
+	return updateDIDCmd
+}
+
+func updateDIDCmd() *cobra.Command {
+	return &cobra.Command{
+		Use:   "update",
+		Short: "Update Orb DID",
+		Long:  "Update Orb DID",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			rootCAs, err := getRootCAs(cmd)
+			if err != nil {
+				return err
+			}
+
+			didURI, err := cmdutils.GetUserSetVarFromString(cmd, didURIFlagName,
+				didURIEnvKey, false)
+			if err != nil {
+				return err
+			}
+
+			sidetreeWriteToken := cmdutils.GetUserSetOptionalVarFromString(cmd, sidetreeWriteTokenFlagName,
+				sidetreeWriteTokenEnvKey)
+
+			domain := cmdutils.GetUserSetOptionalVarFromString(cmd, domainFlagName,
+				domainFileEnvKey)
+
+			didDoc, opts, err := updateDIDOption(didURI, cmd)
+			if err != nil {
+				return err
+			}
+
+			signingKey, err := common.GetKey(cmd, signingKeyFlagName, signingKeyEnvKey, signingKeyFileFlagName,
+				signingKeyFileEnvKey, []byte(cmdutils.GetUserSetOptionalVarFromString(cmd, signingKeyPasswordFlagName,
+					signingKeyPasswordEnvKey)), true)
+			if err != nil {
+				return err
+			}
+
+			nextUpdateKey, err := common.GetKey(cmd, nextUpdateKeyFlagName, nextUpdateKeyEnvKey, nextUpdateKeyFileFlagName,
+				nextUpdateKeyFileEnvKey, nil, false)
+			if err != nil {
+				return err
+			}
+
+			vdr, err := orb.New(&keyRetriever{nextUpdateKey: nextUpdateKey, signingKey: signingKey},
+				orb.WithAuthToken(sidetreeWriteToken), orb.WithDomain(domain),
+				orb.WithTLSConfig(&tls.Config{RootCAs: rootCAs, MinVersion: tls.VersionTLS12}))
+			if err != nil {
+				return err
+			}
+
+			err = vdr.Update(didDoc, opts...)
+			if err != nil {
+				return fmt.Errorf("failed to update did: %w", err)
+			}
+
+			fmt.Printf("successfully updated DID %s", didURI)
+
+			return nil
+		},
+	}
+}
+
+func getSidetreeURL(cmd *cobra.Command) []vdrapi.DIDMethodOption {
+	var opts []vdrapi.DIDMethodOption
+
+	sidetreeURL := cmdutils.GetUserSetOptionalVarFromArrayString(cmd, sidetreeURLFlagName,
+		sidetreeURLEnvKey)
+
+	if len(sidetreeURL) > 0 {
+		opts = append(opts, vdrapi.WithOption(orb.OperationEndpointsOpt, sidetreeURL))
+	}
+
+	return opts
+}
+
+func updateDIDOption(didID string, cmd *cobra.Command) (*ariesdid.Doc, []vdrapi.DIDMethodOption, error) {
+	opts := getSidetreeURL(cmd)
+
+	didDoc, err := getPublicKeys(cmd)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	services, err := getServices(cmd)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	didDoc.ID = didID
+	didDoc.Service = services
+
+	return didDoc, opts, nil
+}
+
+func getServices(cmd *cobra.Command) ([]ariesdid.Service, error) {
+	serviceFile := cmdutils.GetUserSetOptionalVarFromString(cmd, addServiceFileFlagName,
+		addServiceFileEnvKey)
+
+	var svc []ariesdid.Service
+
+	if serviceFile != "" {
+		services, err := common.GetServices(serviceFile)
+		if err != nil {
+			return nil, fmt.Errorf("failed to get services from file %w", err)
+		}
+
+		for i := range services {
+			svc = append(svc, services[i])
+		}
+	}
+
+	return svc, nil
+}
+
+func getPublicKeys(cmd *cobra.Command) (*ariesdid.Doc, error) {
+	publicKeyFile := cmdutils.GetUserSetOptionalVarFromString(cmd, addPublicKeyFileFlagName,
+		addPublicKeyFileEnvKey)
+
+	if publicKeyFile != "" {
+		return common.GetVDRPublicKeysFromFile(publicKeyFile)
+	}
+
+	return &ariesdid.Doc{}, nil
+}
+
+func getRootCAs(cmd *cobra.Command) (*x509.CertPool, error) {
+	tlsSystemCertPoolString := cmdutils.GetUserSetOptionalVarFromString(cmd, tlsSystemCertPoolFlagName,
+		tlsSystemCertPoolEnvKey)
+
+	tlsSystemCertPool := false
+
+	if tlsSystemCertPoolString != "" {
+		var err error
+		tlsSystemCertPool, err = strconv.ParseBool(tlsSystemCertPoolString)
+
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	tlsCACerts := cmdutils.GetUserSetOptionalVarFromArrayString(cmd, tlsCACertsFlagName,
+		tlsCACertsEnvKey)
+
+	return tlsutils.GetCertPool(tlsSystemCertPool, tlsCACerts)
+}
+
+func createFlags(startCmd *cobra.Command) {
+	startCmd.Flags().StringP(didURIFlagName, "", "", didURIFlagUsage)
+	startCmd.Flags().StringP(domainFlagName, "", "", domainFileFlagUsage)
+	startCmd.Flags().StringP(tlsSystemCertPoolFlagName, "", "",
+		tlsSystemCertPoolFlagUsage)
+	startCmd.Flags().StringArrayP(tlsCACertsFlagName, "", []string{}, tlsCACertsFlagUsage)
+	startCmd.Flags().StringP(sidetreeWriteTokenFlagName, "", "", sidetreeWriteTokenFlagUsage)
+	startCmd.Flags().StringP(addPublicKeyFileFlagName, "", "", addPublicKeyFileFlagUsage)
+	startCmd.Flags().StringP(addServiceFileFlagName, "", "", addServiceFlagUsage)
+	startCmd.Flags().StringP(signingKeyFlagName, "", "", signingKeyFlagUsage)
+	startCmd.Flags().StringP(signingKeyFileFlagName, "", "", signingKeyFileFlagUsage)
+	startCmd.Flags().StringP(nextUpdateKeyFlagName, "", "", nextUpdateKeyFlagUsage)
+	startCmd.Flags().StringP(nextUpdateKeyFileFlagName, "", "", nextUpdateKeyFileFlagUsage)
+	startCmd.Flags().StringArrayP(sidetreeURLFlagName, "", []string{}, sidetreeURLFlagUsage)
+	startCmd.Flags().StringP(signingKeyPasswordFlagName, "", "", signingKeyPasswordFlagUsage)
+}
+
+type keyRetriever struct {
+	nextUpdateKey crypto.PublicKey
+	signingKey    crypto.PublicKey
+}
+
+func (k *keyRetriever) GetNextRecoveryPublicKey(didID string) (crypto.PublicKey, error) {
+	return nil, nil
+}
+
+func (k *keyRetriever) GetNextUpdatePublicKey(didID string) (crypto.PublicKey, error) {
+	return k.nextUpdateKey, nil
+}
+
+func (k *keyRetriever) GetSigningKey(didID string, ot orb.OperationType) (crypto.PrivateKey, error) {
+	return k.signingKey, nil
+}
---
+++ cmd/orb-cli/updatedidcmd/updatedid_test.go
@@ -0,0 +1,336 @@
+/*
+Copyright SecureKey Technologies Inc. All Rights Reserved.
+SPDX-License-Identifier: Apache-2.0
+*/
+
+package updatedidcmd
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+const (
+	flag          = "--"
+	publickeyData = `
+[
+ {
+  "id": "key1",
+  "type": "Ed25519VerificationKey2018",
+  "purposes": ["authentication"],
+  "jwkPath": "%s"
+ },
+ {
+  "id": "key2",
+  "type": "JwsVerificationKey2020",
+  "purposes": ["authentication"],
+  "jwkPath": "%s"
+ }
+]`
+
+	jwk1Data = `
+{
+  "kty":"OKP",
+  "crv":"Ed25519",
+  "x":"o1bG1U7G3CNbtALMafUiFOq8ODraTyVTmPtRDO1QUWg",
+  "y":""
+}`
+	jwk2Data = `
+{
+  "kty":"EC",
+  "crv":"P-256",
+  "x":"bGM9aNufpKNPxlkyacU1hGhQXm_aC8hIzSVeKDpwjBw",
+  "y":"PfdmCOtIdVY2B6ucR4oQkt6evQddYhOyHoDYCaI2BJA"
+}`
+
+	pkPEM = `-----BEGIN PUBLIC KEY-----
+MFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAEFoxLiiZZYCh8XOZE0MXUYIgCrwIq
+ho+LGIVUXDNaduiNfpLmk5MXS5Q7WQAMgaJBRyRldIvbrNWqph4DH2gdKQ==
+-----END PUBLIC KEY-----`
+
+	privateKeyPEM = `-----BEGIN EC PRIVATE KEY-----
+Proc-Type: 4,ENCRYPTED
+DEK-Info: AES-256-CBC,A6CD57B60A99920E21D34C0C1E0D90D5
+
+LaYVtZ4SsMthe6NjybSCQa4jOSOtCKEpO3wmbeSBYldJXXrDU4gOSVFiHJ45hTJP
+Q7UGQKWNHeITH8NQlkmcySEKnaI9uyOkcb6TIvklapHCAF8cUf1kCHU10Eo0RTMI
+2tJs7NW6oA4ZNi/o3xYVKVQ1R0lrgQGv9zatOupVPtQ=
+-----END EC PRIVATE KEY-----
+`
+
+	servicesData = `[
+  {
+    "id": "svc1",
+    "type": "type1",
+    "priority": 1,
+    "routingKeys": ["key1"],
+    "recipientKeys": ["key1"],
+    "serviceEndpoint": "http://www.example.com"
+  },
+  {
+    "id": "svc2",
+    "type": "type2",
+    "priority": 2,
+    "routingKeys": ["key2"],
+    "recipientKeys": ["key2"],
+    "serviceEndpoint": "http://www.example.com"
+  }
+]`
+)
+
+func TestMissingArg(t *testing.T) {
+	t.Run("test did uri is missing", func(t *testing.T) {
+		os.Clearenv()
+		cmd := GetUpdateDIDCmd()
+
+		var args []string
+		args = append(args, domainArg()...)
+
+		cmd.SetArgs(args)
+		err := cmd.Execute()
+
+		require.Error(t, err)
+		require.Contains(t, err.Error(), "Neither did-uri (command line flag) nor "+
+			"ORB_CLI_DID_URI (environment variable) have been set.")
+	})
+}
+
+func TestKeyRetriever(t *testing.T) {
+	kr := keyRetriever{nextUpdateKey: []byte("key"), signingKey: []byte("key")}
+
+	_, err := kr.GetNextRecoveryPublicKey("")
+	require.NoError(t, err)
+
+	_, err = kr.GetSigningKey("", 1)
+	require.NoError(t, err)
+
+	_, err = kr.GetNextUpdatePublicKey("")
+	require.NoError(t, err)
+}
+
+func TestKeys(t *testing.T) {
+	t.Run("test signing key empty", func(t *testing.T) {
+		os.Clearenv()
+		cmd := GetUpdateDIDCmd()
+
+		var args []string
+		args = append(args, didURIArg()...)
+		args = append(args, sidetreeURLArg("url")...)
+
+		cmd.SetArgs(args)
+		err := cmd.Execute()
+
+		require.Error(t, err)
+		require.Contains(t, err.Error(), "either key (--signingkey) or key file (--signingkey-file) is required")
+	})
+
+	t.Run("test next update key wrong pem", func(t *testing.T) {
+		os.Clearenv()
+		cmd := GetUpdateDIDCmd()
+
+		file, err := ioutil.TempFile("", "*.json")
+		require.NoError(t, err)
+
+		_, err = file.WriteString(privateKeyPEM)
+		require.NoError(t, err)
+
+		defer func() { require.NoError(t, os.Remove(file.Name())) }()
+
+		var args []string
+		args = append(args, didURIArg()...)
+		args = append(args, domainArg()...)
+		args = append(args, signingKeyPasswordArg()...)
+		args = append(args, signingKeyFileFlagNameArg(file.Name())...)
+		args = append(args, nextUpdateKeyFlagNameArg("w")...)
+
+		cmd.SetArgs(args)
+		err = cmd.Execute()
+
+		require.Error(t, err)
+		require.Contains(t, err.Error(), "public key not found in PEM")
+	})
+}
+
+func TestService(t *testing.T) {
+	t.Run("test services wrong path", func(t *testing.T) {
+		os.Clearenv()
+		cmd := GetUpdateDIDCmd()
+
+		privateKeyFile, err := ioutil.TempFile("", "*.json")
+		require.NoError(t, err)
+
+		_, err = privateKeyFile.WriteString(privateKeyPEM)
+		require.NoError(t, err)
+
+		defer func() { require.NoError(t, os.Remove(privateKeyFile.Name())) }()
+
+		publicKeyFile, err := ioutil.TempFile("", "*.json")
+		require.NoError(t, err)
+
+		_, err = publicKeyFile.WriteString(pkPEM)
+		require.NoError(t, err)
+
+		defer func() { require.NoError(t, os.Remove(publicKeyFile.Name())) }()
+
+		var args []string
+		args = append(args, didURIArg()...)
+		args = append(args, domainArg()...)
+		args = append(args, signingKeyFileFlagNameArg(privateKeyFile.Name())...)
+		args = append(args, nextUpdateKeyFileFlagNameArg(publicKeyFile.Name())...)
+		args = append(args, addServicesFileArg("./wrong")...)
+		args = append(args, signingKeyPasswordArg()...)
+
+		cmd.SetArgs(args)
+		err = cmd.Execute()
+
+		require.Error(t, err)
+		require.Contains(t, err.Error(), "no such file or directory")
+	})
+}
+
+func TestUpdateDID(t *testing.T) {
+	serv := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "{}")
+	}))
+	defer serv.Close()
+
+	privateKeyFile, err := ioutil.TempFile("", "*.json")
+	require.NoError(t, err)
+
+	_, err = privateKeyFile.WriteString(privateKeyPEM)
+	require.NoError(t, err)
+
+	defer func() { require.NoError(t, os.Remove(privateKeyFile.Name())) }()
+
+	publicKeyFile, err := ioutil.TempFile("", "*.json")
+	require.NoError(t, err)
+
+	_, err = publicKeyFile.WriteString(pkPEM)
+	require.NoError(t, err)
+
+	defer func() { require.NoError(t, os.Remove(publicKeyFile.Name())) }()
+
+	servicesFile, err := ioutil.TempFile("", "*.json")
+	require.NoError(t, err)
+
+	_, err = servicesFile.WriteString(servicesData)
+	require.NoError(t, err)
+
+	defer func() { require.NoError(t, os.Remove(servicesFile.Name())) }()
+
+	jwk1File, err := ioutil.TempFile("", "*.json")
+	require.NoError(t, err)
+
+	_, err = jwk1File.WriteString(jwk1Data)
+	require.NoError(t, err)
+
+	defer func() { require.NoError(t, os.Remove(jwk1File.Name())) }()
+
+	jwk2File, err := ioutil.TempFile("", "*.json")
+	require.NoError(t, err)
+
+	_, err = jwk2File.WriteString(jwk2Data)
+	require.NoError(t, err)
+
+	defer func() { require.NoError(t, os.Remove(jwk2File.Name())) }()
+
+	file, err := ioutil.TempFile("", "*.json")
+	require.NoError(t, err)
+
+	_, err = file.WriteString(fmt.Sprintf(publickeyData, jwk1File.Name(), jwk2File.Name()))
+	require.NoError(t, err)
+
+	defer func() { require.NoError(t, os.Remove(file.Name())) }()
+
+	t.Run("test failed to update did", func(t *testing.T) {
+		os.Clearenv()
+		cmd := GetUpdateDIDCmd()
+
+		var args []string
+		args = append(args, didURIArg()...)
+		args = append(args, sidetreeURLArg("wrongurl")...)
+		args = append(args, signingKeyFileFlagNameArg(privateKeyFile.Name())...)
+		args = append(args, nextUpdateKeyFileFlagNameArg(publicKeyFile.Name())...)
+		args = append(args, addServicesFileArg(servicesFile.Name())...)
+		args = append(args, signingKeyPasswordArg()...)
+		args = append(args, addPublicKeyFileArg(file.Name())...)
+
+		cmd.SetArgs(args)
+		err = cmd.Execute()
+
+		require.Error(t, err)
+		require.Contains(t, err.Error(), "failed to update did")
+	})
+}
+
+func TestGetPublicKeys(t *testing.T) {
+	t.Run("test public key invalid path", func(t *testing.T) {
+		os.Clearenv()
+		cmd := GetUpdateDIDCmd()
+
+		var args []string
+		args = append(args, didURIArg()...)
+		args = append(args, domainArg()...)
+		args = append(args, addPublicKeyFileArg("./wrongfile")...)
+
+		cmd.SetArgs(args)
+		err := cmd.Execute()
+
+		require.Error(t, err)
+		require.Contains(t, err.Error(), "open wrongfile: no such file or directory")
+	})
+}
+
+func TestTLSSystemCertPoolInvalidArgsEnvVar(t *testing.T) {
+	os.Clearenv()
+
+	startCmd := GetUpdateDIDCmd()
+
+	require.NoError(t, os.Setenv(tlsSystemCertPoolEnvKey, "wrongvalue"))
+
+	err := startCmd.Execute()
+	require.Error(t, err)
+	require.Contains(t, err.Error(), "invalid syntax")
+}
+
+func domainArg() []string {
+	return []string{flag + domainFlagName, "domain"}
+}
+
+func signingKeyPasswordArg() []string {
+	return []string{flag + signingKeyPasswordFlagName, "123"}
+}
+
+func sidetreeURLArg(value string) []string {
+	return []string{flag + sidetreeURLFlagName, value}
+}
+
+func didURIArg() []string {
+	return []string{flag + didURIFlagName, "did:ex:123"}
+}
+
+func addPublicKeyFileArg(value string) []string {
+	return []string{flag + addPublicKeyFileFlagName, value}
+}
+
+func signingKeyFileFlagNameArg(value string) []string {
+	return []string{flag + signingKeyFileFlagName, value}
+}
+
+func nextUpdateKeyFlagNameArg(value string) []string {
+	return []string{flag + nextUpdateKeyFlagName, value}
+}
+
+func nextUpdateKeyFileFlagNameArg(value string) []string {
+	return []string{flag + nextUpdateKeyFileFlagName, value}
+}
+
+func addServicesFileArg(value string) []string {
+	return []string{flag + addServiceFileFlagName, value}
+}
