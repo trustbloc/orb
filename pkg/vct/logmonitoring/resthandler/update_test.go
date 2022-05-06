ff0ded52353a2cf4466c60ccd5c9c32c91a301ce
Sandra Vrtikapa <sandra.vrtikapa@securekey.com>
2022-05-06 12:37:38 -0400

    feat: Add REST endpoint to deactivate/activate log URL
    Add REST endpoint to deactivate log URL (when it is no longer required to monitor e.g. old log). Also, add endpoint to activate log.
    
    Closes #1276
    
    Signed-off-by: Sandra Vrtikapa <sandra.vrtikapa@securekey.com>

---
+++ pkg/vct/logmonitoring/resthandler/update_test.go
@@ -0,0 +1,256 @@
+/*
+Copyright SecureKey Technologies Inc. All Rights Reserved.
+
+SPDX-License-Identifier: Apache-2.0
+*/
+
+package resthandler
+
+import (
+	"bytes"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+const (
+	testPayload       = `{"activate": ["https://vct.com/log"], "deactivate": ["https://old.com/log"]}`
+	activatePayload   = `{"activate": ["https://vct.com/log", "https://second.com/log"]}`
+	deactivatePayload = `{"deactivate": ["https://vct.com/log", "https://second.com/log"]}`
+)
+
+func TestNewActivate(t *testing.T) {
+	handler := NewUpdateHandler(&mockLogMonitorStore{})
+	require.NotNil(t, handler)
+	require.Equal(t, endpoint, handler.Path())
+	require.Equal(t, http.MethodPost, handler.Method())
+	require.NotNil(t, handler.Handler())
+}
+
+func TestActivate(t *testing.T) {
+	t.Run("success", func(t *testing.T) {
+		handler := NewUpdateHandler(&mockLogMonitorStore{})
+		require.NotNil(t, handler)
+
+		rw := httptest.NewRecorder()
+		req := httptest.NewRequest(http.MethodPost, endpoint, bytes.NewBuffer([]byte(activatePayload)))
+
+		handler.handle(rw, req)
+
+		result := rw.Result()
+		require.Equal(t, http.StatusOK, result.StatusCode)
+		require.NoError(t, result.Body.Close())
+
+		respBytes, err := ioutil.ReadAll(result.Body)
+		require.NoError(t, err)
+		require.Empty(t, respBytes)
+		require.NoError(t, result.Body.Close())
+	})
+
+	t.Run("error - reader error", func(t *testing.T) {
+		handler := NewUpdateHandler(&mockLogMonitorStore{})
+		require.NotNil(t, handler)
+
+		rw := httptest.NewRecorder()
+		req := httptest.NewRequest(http.MethodPost, endpoint, errReader(0))
+
+		handler.handle(rw, req)
+
+		result := rw.Result()
+		require.Equal(t, http.StatusBadRequest, result.StatusCode)
+		require.NoError(t, result.Body.Close())
+
+		respBytes, err := ioutil.ReadAll(result.Body)
+		require.NoError(t, err)
+		require.Equal(t, []byte(badRequestResponse), respBytes)
+		require.NoError(t, result.Body.Close())
+	})
+
+	t.Run("error - unmarshal error", func(t *testing.T) {
+		handler := NewUpdateHandler(&mockLogMonitorStore{})
+		require.NotNil(t, handler)
+
+		rw := httptest.NewRecorder()
+		req := httptest.NewRequest(http.MethodPost, endpoint, bytes.NewBuffer([]byte(activatePayload)))
+
+		errExpected := fmt.Errorf("injected unmarshal error")
+
+		handler.unmarshal = func(bytes []byte, i interface{}) error {
+			return errExpected
+		}
+
+		handler.handle(rw, req)
+
+		result := rw.Result()
+		require.Equal(t, http.StatusBadRequest, result.StatusCode)
+		require.NoError(t, result.Body.Close())
+
+		respBytes, err := ioutil.ReadAll(result.Body)
+		require.NoError(t, err)
+		require.Equal(t, []byte(badRequestResponse), respBytes)
+		require.NoError(t, result.Body.Close())
+	})
+
+	t.Run("error - parse URL error", func(t *testing.T) {
+		handler := NewUpdateHandler(&mockLogMonitorStore{})
+		require.NotNil(t, handler)
+
+		invalidPayload := []byte(`{"activate": [":InvalidURL"]}`)
+
+		rw := httptest.NewRecorder()
+		req := httptest.NewRequest(http.MethodPost, endpoint, bytes.NewBuffer(invalidPayload))
+
+		handler.handle(rw, req)
+
+		result := rw.Result()
+		require.Equal(t, http.StatusBadRequest, result.StatusCode)
+		require.NoError(t, result.Body.Close())
+
+		respBytes, err := ioutil.ReadAll(result.Body)
+		require.NoError(t, err)
+		require.Equal(t, []byte(badRequestResponse), respBytes)
+		require.NoError(t, result.Body.Close())
+	})
+
+	t.Run("error - log monitor store error", func(t *testing.T) {
+		handler := NewUpdateHandler(&mockLogMonitorStore{Err: fmt.Errorf("log monitor store error")})
+		require.NotNil(t, handler)
+
+		rw := httptest.NewRecorder()
+		req := httptest.NewRequest(http.MethodPost, endpoint, bytes.NewBuffer([]byte(activatePayload)))
+
+		handler.handle(rw, req)
+
+		result := rw.Result()
+		require.Equal(t, http.StatusInternalServerError, result.StatusCode)
+		require.NoError(t, result.Body.Close())
+
+		respBytes, err := ioutil.ReadAll(result.Body)
+		require.NoError(t, err)
+		require.Equal(t, []byte(internalServerErrorResponse), respBytes)
+		require.NoError(t, result.Body.Close())
+	})
+}
+
+func TestDeactivate(t *testing.T) {
+	t.Run("success", func(t *testing.T) {
+		handler := NewUpdateHandler(&mockLogMonitorStore{})
+		require.NotNil(t, handler)
+
+		rw := httptest.NewRecorder()
+		req := httptest.NewRequest(http.MethodPost, endpoint, bytes.NewBuffer([]byte(deactivatePayload)))
+
+		handler.handle(rw, req)
+
+		result := rw.Result()
+		require.Equal(t, http.StatusOK, result.StatusCode)
+		require.NoError(t, result.Body.Close())
+
+		respBytes, err := ioutil.ReadAll(result.Body)
+		require.NoError(t, err)
+		require.Empty(t, respBytes)
+		require.NoError(t, result.Body.Close())
+	})
+
+	t.Run("error - reader error", func(t *testing.T) {
+		handler := NewUpdateHandler(&mockLogMonitorStore{})
+		require.NotNil(t, handler)
+
+		rw := httptest.NewRecorder()
+		req := httptest.NewRequest(http.MethodPost, endpoint, errReader(0))
+
+		handler.handle(rw, req)
+
+		result := rw.Result()
+		require.Equal(t, http.StatusBadRequest, result.StatusCode)
+		require.NoError(t, result.Body.Close())
+
+		respBytes, err := ioutil.ReadAll(result.Body)
+		require.NoError(t, err)
+		require.Equal(t, []byte(badRequestResponse), respBytes)
+		require.NoError(t, result.Body.Close())
+	})
+
+	t.Run("error - parse URL error", func(t *testing.T) {
+		handler := NewUpdateHandler(&mockLogMonitorStore{})
+		require.NotNil(t, handler)
+
+		invalidPayload := []byte(`{"deactivate": [":InvalidURL"]}`)
+
+		rw := httptest.NewRecorder()
+		req := httptest.NewRequest(http.MethodPost, endpoint, bytes.NewBuffer(invalidPayload))
+
+		handler.handle(rw, req)
+
+		result := rw.Result()
+		require.Equal(t, http.StatusBadRequest, result.StatusCode)
+		require.NoError(t, result.Body.Close())
+
+		respBytes, err := ioutil.ReadAll(result.Body)
+		require.NoError(t, err)
+		require.Equal(t, []byte(badRequestResponse), respBytes)
+		require.NoError(t, result.Body.Close())
+	})
+
+	t.Run("error - log monitor store error", func(t *testing.T) {
+		handler := NewUpdateHandler(&mockLogMonitorStore{Err: fmt.Errorf("log monitor store error")})
+		require.NotNil(t, handler)
+
+		rw := httptest.NewRecorder()
+		req := httptest.NewRequest(http.MethodPost, endpoint, bytes.NewBuffer([]byte(deactivatePayload)))
+
+		handler.handle(rw, req)
+
+		result := rw.Result()
+		require.Equal(t, http.StatusInternalServerError, result.StatusCode)
+		require.NoError(t, result.Body.Close())
+
+		respBytes, err := ioutil.ReadAll(result.Body)
+		require.NoError(t, err)
+		require.Equal(t, []byte(internalServerErrorResponse), respBytes)
+		require.NoError(t, result.Body.Close())
+	})
+}
+
+func TestActivateAndDeactivate(t *testing.T) {
+	t.Run("success", func(t *testing.T) {
+		handler := NewUpdateHandler(&mockLogMonitorStore{})
+		require.NotNil(t, handler)
+
+		rw := httptest.NewRecorder()
+		req := httptest.NewRequest(http.MethodPost, endpoint, bytes.NewBuffer([]byte(testPayload)))
+
+		handler.handle(rw, req)
+
+		result := rw.Result()
+		require.Equal(t, http.StatusOK, result.StatusCode)
+		require.NoError(t, result.Body.Close())
+
+		respBytes, err := ioutil.ReadAll(result.Body)
+		require.NoError(t, err)
+		require.Empty(t, respBytes)
+		require.NoError(t, result.Body.Close())
+	})
+}
+
+type errReader int
+
+func (errReader) Read(p []byte) (n int, err error) {
+	return 0, fmt.Errorf("reader error")
+}
+
+type mockLogMonitorStore struct {
+	Err error
+}
+
+func (m *mockLogMonitorStore) Activate(_ string) error {
+	return m.Err
+}
+
+func (m *mockLogMonitorStore) Deactivate(_ string) error {
+	return m.Err
+}
